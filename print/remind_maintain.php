e67a042b22ae5ad8281ce6813e71ca5febcb50b1
Frank <68852874@qq.com>
2015-11-11 18:50:41 +0800

    add the function to judge whether the same email has been sent

--- print/remind_maintain.php
+++ print/remind_maintain.php
@@ -11,6 +11,10 @@
 	define('IN_SUSTC', true);
 	define('CURSCRIPT', 'print');
 	define('SC_ROOT', substr(__FILE__, 0, -25));
+
+	define('CLOUDPRINT_NODE_NOT_REPAIRING', 0);
+	define('CLOUDPRINT_NODE_DURING_REPAIRING', 1);
+
 	require_once SC_ROOT.'src/service/cloudprint.php';
 	require_once SC_ROOT.'src/core.php';
 	require_once SC_ROOT."src/func/send_mail.php"; 
@@ -18,21 +22,30 @@
 
 	$sustc = new core();
 	$cloudprint = new cloudprint($sustc);
-	//fetch the nodes_name offline or with errors
-	$error_printers=array();
 
+	//fetch the nodes_name offline or with errors but not been reminded in email
+	$error_printers=array();
 	foreach ($cloudprint->nodes as $nodes) {
-		if ($nodes['status'] == 0) {
-			$error_printers[]= array('name'=>$nodes['name'] , 'err'=>CLOUDPRINT_NODE_STATUS_OFFLINE);
+		if ($nodes['status'] == CLOUDPRINT_NODE_STATUS_OFFLINE ) {
+			if ($nodes['under_repairing']==0) {
+			$error_printers[]= array('id'=>$nodes['id'],'name'=>$nodes['name'], 'err'=>'Offline');
+			write_status_to_repairing($nodes['id'],1);
+			}
+		}elseif ($nodes['status'] == CLOUDPRINT_NODE_STATUS_PROBLEM) {
+			if ($nodes['under_repairing']==0) {
+			$error_printers[]= array('id'=>$nodes['id'],'name'=>$nodes['name'], 'err'=>'Problem');
+			write_status_to_repairing($nodes['id'],1);
+			}
+		}else{
+			if ($nodes['under_repairing']!=0) {
+			write_status_to_repairing($nodes['id'],0);
+			}
 		}
 	}
 	unset($nodes);
 
 
-	foreach ($cloudprint->nodestatus['err'] as $err_nodename) {
-		$error_printers[]= array('name'=>$err_nodename, 'err'=>CLOUDPRINT_NODE_STATUS_PROBLEM);
-	}
-	unset($err);
+	//send email
 	if (count($error_printers)>0) {
 			$template= file_get_contents(SC_ROOT.'\template\default\print/reminder_email.htm');
 
@@ -57,10 +70,13 @@ Bodyhead;
 				echo 'Something Wrong with printers.But fail to send an email.';
 			}
 	}else{
-		echo 'No printer is down.';
+		echo 'No printer is down Or an email has been sent.';
 	}
 
-
+	//write database
+	function write_status_to_repairing ($node_id,$isunder_repairing){
+		DB::update('print_node',array('under_repairing'=>$isunder_repairing),array('id' =>$node_id));
+	}
 	//array in the $cloudprint
 	/*
 	    [nodes] => Array
